e96c1a5f8bda71620bfcf8c9de3031c72cf1f09d
Warner Iveris <warner@Warners-MacBook-Pro.local>
2020-12-16 20:52:57 -0800

    first real commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Building a rainbow "Guitar For Kids!" header 
+const rainbow = ['red', 'orange', 'yellow', 'blue', 'indigo', 'violet'];
+const kidsHeader = "Guitar For Kids!";
+const guitar4kids = document.querySelector('#g4k');
+// creating spans for each letter
+for (let letter in kidsHeader) {
+    let span = document.createElement('span');
+    span.innerText = kidsHeader[letter];
+    guitar4kids.appendChild(span);
+}
+// changing the color of each letter
+const guitar4kidsHeader = document.querySelectorAll('#g4k span');
+let count = 0;
+for (let i = 0; i < guitar4kidsHeader.length; i++) {
+    let temp = guitar4kidsHeader[i];
+    if (temp.innerText === ' ') { continue; }
+    else { temp.style.color = rainbow[count]; }
+    if (count + 1 === rainbow.length) { count = 0; }
+    else { count++; }
+}
